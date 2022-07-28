08eff582877df9ad96f9b59a531dd63d3952ded1
emmanuelByte <emanuel4mechie@gmail.com>
2022-07-28 15:44:49 +0100

    [Feature] - Setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigService } from './utils/database/typeorm-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    })
+
+
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
