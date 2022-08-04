ead9c1609b5265bf313c6c9776eda8276ce9d509
emmanuelByte <emanuel4mechie@gmail.com>
2022-08-04 09:48:02 +0100

    Added docstring to function

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,12 @@ export class AppModule {
     AppModule.port = this.configService.get('PORT');
   }
 
-  static getBaseUrl(app: INestApplication) {
+  /**
+   * 
+   * @param app  - Nest application
+   * @returns string - base url
+   */
+  static getBaseUrl(app: INestApplication): string {
     let baseUrl = app.getHttpServer().address().address;
    if (baseUrl === '0.0.0.0' || baseUrl === '::') {
      return baseUrl = 'localhost';
